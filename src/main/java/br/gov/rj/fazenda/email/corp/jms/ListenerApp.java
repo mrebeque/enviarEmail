908c400243dab428f2ac8b28b648d7196e84d62c
Marcelo Rebeque <marcelo.rebeque@gmail.com>
2022-10-21 20:54:12 -0300

    Implataçao do envio de email!

--- src/main/java/br/gov/rj/fazenda/email/corp/jms/ListenerApp.java
+++ src/main/java/br/gov/rj/fazenda/email/corp/jms/ListenerApp.java
@@ -11,6 +11,7 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
+import br.gov.rj.fazenda.email.corp.security.services.EmailService;
 import br.gov.rj.fazenda.email.corp.vo.Email;
 
 @Component
@@ -24,30 +25,20 @@ public class ListenerApp {
 		@Value("${email.corp.file_dir}")
 		private String fileDir;
 		
+		@Autowired
+		private EmailService emailService;
+		
 	    @Autowired
 	    public ListenerApp(JmsTemplate jmsTemplate) {
 			this.jmsTemplate = jmsTemplate;
-	    }
-	    
+	    }	    
 		
 	    @JmsListener(destination = "${email.corp.mq.queue}")
-	    public void onReceiverTopic(Email objMsg ) {
+	    public void onReceiverTopic(Email email ) {
 
-		      if (objMsg instanceof Email)
-		    	  System.out.println("E email");
-		      
-		      Email email = (Email) objMsg;
-		      
-		    	System.out.println("Email: " + email.getCorpo());
-	       		System.out.println(email.getAnexos().getNome());
-		       	for (int i = 0; i < email.getAnexos().getArquivo().size(); i++) {
-		       		Path path = Paths.get(fileDir +"/anexo-"+i+".pdf");
-						try {
-							Files.write(path, email.getAnexos().getArquivo().get(i));
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-		       	}	    	
+	    	System.out.println("Email: " + email.getSubject());
+
+	    	emailService.sendMail(email);
 	    
 	    
 	    }
